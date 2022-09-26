2727dc7a5197e1a3a716bc6bb05c30633e003ea4
EnzoEduardoBruno <enzoeduardobruno@gmail.com>
2022-09-26 16:10:00 -0300

    Se crean nuevas funciones para experiencia laboral

--- src/app/model/experiencia.ts
+++ src/app/model/experiencia.ts
@@ -1,10 +1,14 @@
 export class Experiencia {
     id? : number;
     nombreE : string;
+    timeE : string;
+    puestoE : string;
     descripcionE : string;
 
-    constructor(nombreE: string, descripcionE: string){
+    constructor(nombreE: string, timeE: string, puestoE: string, descripcionE: string){
         this.nombreE = nombreE;
+        this.timeE = timeE;
+        this.puestoE = puestoE;
         this.descripcionE = descripcionE;
     }
 }
