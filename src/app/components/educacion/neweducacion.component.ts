4a88ea1fe2a03a7713112a8f265d3b10af011b9c
EnzoEduardoBruno <enzoeduardobruno@gmail.com>
2022-09-26 23:40:17 -0300

    Se crean nuevas funciones para educacion

--- src/app/components/educacion/neweducacion.component.ts
+++ src/app/components/educacion/neweducacion.component.ts
@@ -10,6 +10,8 @@ import { EducacionService } from 'src/app/service/educacion.service';
 })
 export class NeweducacionComponent implements OnInit {
   nombreE: string;
+  timeE: string = '';
+  tituloE: string = '';
   descripcionE: string;
 
   constructor(private educacionS: EducacionService, private router: Router) { }
@@ -18,7 +20,7 @@ export class NeweducacionComponent implements OnInit {
   }
 
   onCreate(): void{
-    const educacion = new Educacion(this.nombreE, this.descripcionE);
+    const educacion = new Educacion(this.nombreE,  this.timeE, this.tituloE,this.descripcionE);
     this.educacionS.save(educacion).subscribe(
       data =>{
         alert("Educacion añadida correctamente");
--- src/app/model/educacion.ts
+++ src/app/model/educacion.ts
@@ -1,10 +1,14 @@
 export class Educacion {
     id?: number;
     nombreE: string;
+    timeE: string;
+    tituloE: string;
     descripcionE: string;
 
-    constructor(nombreE: string, descripcionE: string){
+    constructor(nombreE: string, timeE: string, tituloE: string,descripcionE: string){
         this.nombreE = nombreE;
+        this.timeE = timeE;
+        this.tituloE = tituloE;
         this.descripcionE = descripcionE;
     }
 }
